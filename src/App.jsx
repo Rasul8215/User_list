0f4b5858b3477031cfba58049ed7edff5ef42918
Rasul8215 <rasulrasul8215@gmail.com>
2023-02-18 20:50:27 +0530

    search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import CreateUserModal from "./components/CreateUserModal";
 import InputField from "./components/InputField";
 import UserDetailsCard from "./components/UserDetailsCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import  Button  from "./components/Button"
 
 function App() {
   const [showUserModal,setShowUserModal]=useState(false)
   const [users,setUsers]=useState([])
  // [{name:"sfdvj",dob:new Date(),gender:"Male",city:"tvm",id:new Date()}]
-
+  const [searchUsers,setSearchUsers]=useState("")
+  const [displayUsers,setDisplayUsers]=useState([])
+  useEffect(()=>{
+    setDisplayUsers(users.filter((user)=>{
+      return user.name.includes(searchUsers)
+    }))
+  },[users,searchUsers])
   return (
     <div className="container mt-5">
       <div className="input-group rounded">
@@ -18,6 +24,9 @@ function App() {
           placeholder="Search"
           aria-label="Search"
           aria-describedby="search-addon"
+          onChange={(e)=>{
+            setSearchUsers(e.target.value)
+          }}
         />
       </div>
       <div className="d-flex justify-content-end mt-3">
@@ -26,10 +35,11 @@ function App() {
           setShowUserModal(true)
         }} >Add User</Button>
       </div>
+     
       <CreateUserModal show={showUserModal} closeModal={setShowUserModal} createUser={setUsers}/>
       <div className="mt-5 d-flex flex-wrap gap-4 ">
         
-        {users.map((user)=>{
+        {displayUsers.map((user)=>{
           return <UserDetailsCard name={user.name} dob={new Date(user.dob)} gender={user.gender} city={user.city} key={user.id} 
           deleteUser={setUsers} id={user.id}/>
         })}
